4db81d263cdc7677468c74d91508d3e4c6d26fd4
Walter Diaz <wdiaz503@gmail.com>
2024-03-27 02:52:14 +0000

    removed commented out code

--- src/DataFixtures/ProductFixtures.php
+++ src/DataFixtures/ProductFixtures.php
@@ -6,16 +6,18 @@ use App\Factory\ProductFactory;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
+/**
+ * Load products onto the database
+ */
 class ProductFixtures extends Fixture
 {
+    /**
+     * @param ObjectManager $manager
+     * @return void
+     */
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
-
         $product = ProductFactory::createMany(10);
-
-
         $manager->flush();
     }
 }
