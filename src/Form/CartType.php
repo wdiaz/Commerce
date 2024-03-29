9a73f30a00d7f07ff850115095438173bb44de50
Walter Diaz <wdiaz503@gmail.com>
2024-03-29 01:30:19 +0000

    removed references to product in cart

--- src/Form/CartType.php
+++ src/Form/CartType.php
@@ -3,11 +3,9 @@
 namespace App\Form;
 
 use App\Entity\Cart;
-use App\Entity\Product;
 use App\Entity\User;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\UuidType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -16,13 +14,6 @@ class CartType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            /*->add('createdAt', null, [
-                'widget' => 'single_text',
-            ])*/
-            ->add('product', EntityType::class, [
-                'class' => Product::class,
-                'choice_label' => 'id',
-            ])
             ->add('cartUser', EntityType::class, [
                 'class' => User::class,
                 'choice_label' => 'id',
