17a842d0a1189614ec6ee578647427c7163e03bf
Walter Diaz <wdiaz503@gmail.com>
2024-03-29 03:47:06 +0000

    added default operations

--- src/Entity/Cart.php
+++ src/Entity/Cart.php
@@ -3,13 +3,29 @@
 namespace App\Entity;
 
 use ApiPlatform\Metadata\ApiResource;
+use ApiPlatform\Metadata\Delete;
+use ApiPlatform\Metadata\Get;
+use ApiPlatform\Metadata\GetCollection;
+use ApiPlatform\Metadata\Patch;
+use ApiPlatform\Metadata\Post;
+use ApiPlatform\Metadata\Put;
 use App\Repository\CartRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: CartRepository::class)]
-#[ApiResource]
+#[ApiResource(
+    description: "Main Cart Entity Holder",
+    operations: [
+        new GetCollection(),
+        new Post(),
+        new Get(),
+        new Put(),
+        new Delete(),
+        new Patch(),
+    ]
+)]
 class Cart
 {
     use TimestampableTrait;
