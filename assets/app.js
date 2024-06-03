5c2bbc378ae7a4af3a51e971f3f9ec79293271ee
Walter Diaz <wdiaz503@gmail.com>
2024-06-03 04:06:49 +0000

    fully working tailwing setup

--- assets/app.js
+++ assets/app.js
@@ -1,16 +1,9 @@
 import { registerReactControllerComponents } from '@symfony/ux-react';
-import './bootstrap.js';
+
 import './css/app.css';
 
-import 'bootstrap/dist/js/bootstrap.bundle.min.js';
-
-import Dropzone from "dropzone";
-
-const $  = require("jquery");
-window.$ = $;
 
 
 
-Dropzone.autoDiscover = false;
 
 registerReactControllerComponents(require.context('./react/controllers', true, /\.(j|t)sx?$/));
