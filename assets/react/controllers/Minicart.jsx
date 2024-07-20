e169c0afd12a33a07d063e4398be5e30effd248e
Walter Diaz <wdiaz503@gmail.com>
2024-07-20 22:03:47 +0000

    added link to cart

--- assets/react/controllers/Minicart.jsx
+++ assets/react/controllers/Minicart.jsx
@@ -45,7 +45,7 @@ export default function () {
 
     return (
     <div className="flex items-center">
-        <a href="#" className="flex items-center space-x-2 text-white">
+        <a href="/cart" className="flex items-center space-x-2 text-white">
             <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24"
                  stroke="currentColor">
                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"
