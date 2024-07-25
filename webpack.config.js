cce085d32c6cbba3af1f3d8dcbf5d40bf81ec50c
Walter Diaz <wdiaz503@gmail.com>
2024-07-25 00:27:05 +0000

    updated webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -13,6 +13,11 @@ Encore
     .setPublicPath('/build')
     // only needed for CDN's or subdirectory deploy
     //.setManifestKeyPrefix('build/')
+    .copyFiles({
+        from: './assets/img',
+        to: 'images/[path][name].[ext]',
+    })
+
 
     /*
      * ENTRY CONFIG
