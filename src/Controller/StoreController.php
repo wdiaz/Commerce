c3b552e1b5bea746542ed8b03e39d1ad9a9f8ec4
Walter Diaz <wdiaz503@gmail.com>
2024-11-03 19:55:47 +0000

    allow products to be displayed on the merchant pages

--- src/Controller/StoreController.php
+++ src/Controller/StoreController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\Merchant;
+use App\Repository\ProductRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -19,10 +20,12 @@ class StoreController extends AbstractController
     }
 
     #[Route('/{slug}', name: 'app_store_view', methods: ['GET'])]
-    public function view(Merchant $merchant): Response
+    public function view(Merchant $merchant, ProductRepository $productRepository): Response
     {
+        $products = $productRepository->findBy(['merchant' => $merchant]);
         return $this->render('store/index.html.twig', [
             'controller_name' => 'StoreController',
+            'products' => $products,
             'merchant' => $merchant,
         ]);
     }
