d34cdb5fe248eb39b307392a8132de82661f216f
Walter Diaz <wdiaz503@gmail.com>
2024-11-03 18:52:42 +0000

    added tinymce

--- webpack.config.js
+++ webpack.config.js
@@ -17,6 +17,22 @@ Encore
         from: './assets/img',
         to: 'images/[path][name].[ext]',
     })
+    .copyFiles({
+        from: './node_modules/tinymce/skins', // Copy skins
+        to: 'tinymce/skins/[path][name].[ext]',
+    })
+    .copyFiles({
+        from: './node_modules/tinymce/icons', // Copy icons
+        to: 'tinymce/icons/[path][name].[ext]',
+    })
+    .copyFiles({
+        from: './node_modules/tinymce/themes', // Copy themes
+        to: 'tinymce/themes/[path][name].[ext]',
+    })
+    .copyFiles({
+        from: './node_modules/tinymce/plugins', // Copy plugins
+        to: 'tinymce/plugins/[path][name].[ext]',
+    })
 
 
     /*
@@ -26,6 +42,7 @@ Encore
      * and one CSS file (e.g. app.css) if your JavaScript imports CSS.
      */
     .addEntry('app', './assets/app.js')
+    .addEntry('new_product', './assets/new_product.js')
     //.addEntry('related_products', './assets/js/components/pages/product/view/RelatedProducts')
 
     // When enabled, Webpack "splits" your files into smaller pieces for greater optimization.
