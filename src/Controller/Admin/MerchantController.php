3067f5244f3ea29cd9437955159e4af8f3671044
Walter Diaz <wdiaz503@gmail.com>
2024-11-10 05:07:00 +0000

    refactored merchant

--- src/Controller/Admin/MerchantController.php
+++ src/Controller/Admin/MerchantController.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Controller;
+namespace App\Controller\Admin;
 
 use App\Entity\Merchant;
 use App\Form\MerchantType;
@@ -10,20 +10,19 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
-use Symfony\Component\Uid\Uuid;
 
-#[Route('/merchant')]
+#[Route('/admin/merchant')]
 class MerchantController extends AbstractController
 {
-    #[Route('/', name: 'app_merchant_index', methods: ['GET'])]
+    #[Route('/', name: 'app_admin_merchant_index', methods: ['GET'])]
     public function index(MerchantRepository $merchantRepository): Response
     {
-        return $this->render('merchant/index.html.twig', [
+        return $this->render('admin/merchant/index.html.twig', [
             'merchants' => $merchantRepository->findAll(),
         ]);
     }
 
-    #[Route('/new', name: 'app_merchant_new', methods: ['GET', 'POST'])]
+    #[Route('/new', name: 'app_admin_merchant_new', methods: ['GET', 'POST'])]
     public function new(Request $request, EntityManagerInterface $entityManager): Response
     {
         $merchant = new Merchant();
@@ -33,24 +32,24 @@ class MerchantController extends AbstractController
             $entityManager->persist($merchant);
             $entityManager->flush();
 
-            return $this->redirectToRoute('app_merchant_index', [], Response::HTTP_SEE_OTHER);
+            return $this->redirectToRoute('app_admin_merchant_index', [], Response::HTTP_SEE_OTHER);
         }
 
-        return $this->render('merchant/new.html.twig', [
+        return $this->render('admin/merchant/new.html.twig', [
             'merchant' => $merchant,
             'form' => $form,
         ]);
     }
 
-    #[Route('/{id}', name: 'app_merchant_show', methods: ['GET'])]
+    #[Route('/{id}', name: 'app_admin_merchant_show', methods: ['GET'])]
     public function show(Merchant $merchant): Response
     {
-        return $this->render('merchant/show.html.twig', [
+        return $this->render('admin/merchant/show.html.twig', [
             'merchant' => $merchant,
         ]);
     }
 
-    #[Route('/{id}/edit', name: 'app_merchant_edit', methods: ['GET', 'POST'])]
+    #[Route('/{id}/edit', name: 'app_admin_merchant_edit', methods: ['GET', 'POST'])]
     public function edit(Request $request, Merchant $merchant, EntityManagerInterface $entityManager): Response
     {
         $form = $this->createForm(MerchantType::class, $merchant);
@@ -59,16 +58,16 @@ class MerchantController extends AbstractController
         if ($form->isSubmitted() && $form->isValid()) {
             $entityManager->flush();
 
-            return $this->redirectToRoute('app_merchant_index', [], Response::HTTP_SEE_OTHER);
+            return $this->redirectToRoute('app_admin_merchant_index', [], Response::HTTP_SEE_OTHER);
         }
 
-        return $this->render('merchant/edit.html.twig', [
+        return $this->render('admin/merchant/edit.html.twig', [
             'merchant' => $merchant,
             'form' => $form,
         ]);
     }
 
-    #[Route('/{id}', name: 'app_merchant_delete', methods: ['POST'])]
+    #[Route('/{id}', name: 'app_admin_merchant_delete', methods: ['POST'])]
     public function delete(Request $request, Merchant $merchant, EntityManagerInterface $entityManager): Response
     {
         if ($this->isCsrfTokenValid('delete'.$merchant->getId(), $request->getPayload()->get('_token'))) {
@@ -76,6 +75,6 @@ class MerchantController extends AbstractController
             $entityManager->flush();
         }
 
-        return $this->redirectToRoute('app_merchant_index', [], Response::HTTP_SEE_OTHER);
+        return $this->redirectToRoute('app_admin_merchant_index', [], Response::HTTP_SEE_OTHER);
     }
 }
