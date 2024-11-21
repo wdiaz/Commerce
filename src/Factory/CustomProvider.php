c4a91f77b31d21688d1a00ad25d42f80c8bfff14
Walter Diaz <wdiaz503@gmail.com>
2024-11-21 07:20:17 +0000

    updated zendtruck and logic

--- src/Factory/CustomProvider.php
+++ src/Factory/CustomProvider.php
@@ -2,26 +2,18 @@
 
 namespace App\Factory;
 
-use Faker\Provider\Base;
 use Faker\Generator;
+use Faker\Provider\Base;
 use Symfony\Component\Yaml\Exception\RuntimeException;
 use Symfony\Component\Yaml\Yaml;
 
 class CustomProvider extends Base
 {
-
     private const DIRECTORY = '/config/';
     private const PRODUCTS = 'products.yml';
-    protected static array $departments = ["Books", "Movies", "Music", "Games", "Electronics", "Computers", "Home", "Garden", "Tools", "Grocery", "Health", "Beauty", "Toys", "Kids", "Baby", "Clothing", "Shoes", "Jewelry", "Sports", "Outdoors", "Automotive", "Industrial"];
-
+    protected static array $departments = ['Books', 'Movies', 'Music', 'Games', 'Electronics', 'Computers', 'Home', 'Garden', 'Tools', 'Grocery', 'Health', 'Beauty', 'Toys', 'Kids', 'Baby', 'Clothing', 'Shoes', 'Jewelry', 'Sports', 'Outdoors', 'Automotive', 'Industrial'];
 
     protected static array $productNames = [];
-
-    /*protected static $productNames = [
-        'adjective' => ['Small', 'Ergonomic', 'Rustic', 'Intelligent', 'Gorgeous', 'Incredible', 'Fantastic', 'Practical', 'Sleek', 'Awesome', 'Enormous', 'Mediocre', 'Synergistic', 'Heavy Duty', 'Lightweight', 'Aerodynamic', 'Durable'],
-        'material' => ['Steel', 'Wooden', 'Concrete', 'Plastic', 'Cotton', 'Granite', 'Rubber', 'Leather', 'Silk', 'Wool', 'Linen', 'Marble', 'Iron', 'Bronze', 'Copper', 'Aluminum', 'Paper'],
-        'product' => ['Chair', 'Car', 'Computer', 'Gloves', 'Pants', 'Shirt', 'Table', 'Shoes', 'Hat', 'Plate', 'Knife', 'Bottle', 'Coat', 'Lamp', 'Keyboard', 'Bag', 'Bench', 'Clock', 'Watch', 'Wallet'],
-    ];*/
 
     public function __construct(Generator $generator)
     {
@@ -32,11 +24,11 @@ class CustomProvider extends Base
 
     private function loadProducts(): void
     {
-        if(empty(self::$productNames)) {
-            if(false === is_readable(__DIR__ . self::DIRECTORY . self::PRODUCTS )) {
-                throw new RuntimeException(sprintf("%s file is not readable", self::PRODUCTS));
+        if (empty(self::$productNames)) {
+            if (false === is_readable(__DIR__.self::DIRECTORY.self::PRODUCTS)) {
+                throw new RuntimeException(sprintf('%s file is not readable', self::PRODUCTS));
             }
-            $products = Yaml::parseFile(__DIR__ . self::DIRECTORY . self::PRODUCTS );
+            $products = Yaml::parseFile(__DIR__.self::DIRECTORY.self::PRODUCTS);
             self::$productNames = $products['products'];
         }
     }
@@ -44,6 +36,7 @@ class CustomProvider extends Base
     protected function generate()
     {
         $size = count(self::$productNames);
+
         return rand(0, $size - 1);
     }
 
@@ -55,7 +48,6 @@ class CustomProvider extends Base
             if (in_array($randomIndex, $holder)) {
                 $randomIndex = $this->generate();
             }
-
         } while (in_array($randomIndex, $holder));
 
         $holder[$randomIndex] = $randomIndex;
@@ -65,13 +57,14 @@ class CustomProvider extends Base
 
     public function name()
     {
-        return $this->generator->randomElements(self::$productNames,1, false)[0]['name'];
+        return $this->generator->randomElements(self::$productNames, 1, false)[0]['name'];
     }
 
     public function description()
     {
         return $this->generator->randomElements(self::$productNames)[0]['description'];
     }
+
     public function category()
     {
         return $this->randomElement(self::$departments);
