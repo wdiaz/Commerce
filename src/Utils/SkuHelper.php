90307e93c5881d76580a6366da2e806d3b4a88b5
Walter Diaz <wdiaz503@gmail.com>
2024-11-21 07:47:09 +0000

    coding standard

--- src/Utils/SkuHelper.php
+++ src/Utils/SkuHelper.php
@@ -2,8 +2,16 @@
 
 namespace App\Utils;
 
+/**
+ * Allows search to redirect to product page directly.
+ */
 class SkuHelper
 {
+    /**
+     * @param string $query
+     *
+     * @return bool
+     */
     public static function isSku(string $query): bool
     {
         return 1 === preg_match('/^[A-Z0-9]{10}$|^\d{10}$/', $query);
