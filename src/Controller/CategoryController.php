a579b325f1bd7721dfdc8729c44194dafce08f38
Walter Diaz <wdiaz503@gmail.com>
2024-12-26 23:59:08 +0000

    updated to routing attributes

--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -3,14 +3,11 @@
 namespace App\Controller;
 
 use App\Entity\Category;
-use App\Form\CategoryType;
 use App\Repository\CategoryRepository;
 use App\Repository\ProductRepository;
-use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\Attribute\Route;
 
 #[Route('/c')]
 class CategoryController extends AbstractController
--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -6,7 +6,7 @@ use App\Entity\Product;
 use App\Form\ProductActionType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\Attribute\Route;
 
 #[Route('/product')]
 class ProductController extends AbstractController
