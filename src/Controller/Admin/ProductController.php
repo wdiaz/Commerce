25509caa2a89a13db9c1d9d4c83e53b42291b477
Walter Diaz <wdiaz503@gmail.com>
2025-01-18 19:18:10 +0000

    cs fixer

--- src/Controller/Admin/ProductController.php
+++ src/Controller/Admin/ProductController.php
@@ -18,11 +18,11 @@ use Symfony\Component\Routing\Attribute\Route;
 class ProductController extends AbstractController
 {
     private PaginationService $paginationService;
+
     public function __construct(PaginationService $paginationService)
     {
         $this->paginationService = $paginationService;
     }
-
 
     #[Route('/', name: 'app_admin_product_index', methods: ['GET'])]
     public function index(Request $request, ProductRepository $productRepository): Response
@@ -80,7 +80,7 @@ class ProductController extends AbstractController
     #[Route('/{id}', name: 'app_admin_product_delete', methods: ['POST'])]
     public function delete(Request $request, Product $product, EntityManagerInterface $entityManager): Response
     {
-        if ($this->isCsrfTokenValid('delete' . $product->getId(), $request->request->get('_token'))) {
+        if ($this->isCsrfTokenValid('delete'.$product->getId(), $request->request->get('_token'))) {
             $entityManager->remove($product);
             $entityManager->flush();
         }
