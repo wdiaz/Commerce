63b799962288eca81924f1092d79dd1eb0c9290a
Walter Diaz <wdiaz503@gmail.com>
2025-07-16 05:48:30 +0000

    added checkout placeholder functionality

--- src/Controller/CheckoutController.php
+++ src/Controller/CheckoutController.php
@@ -2,17 +2,27 @@
 
 namespace App\Controller;
 
+use App\Repository\CartRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 class CheckoutController extends AbstractController
 {
     #[Route('/checkout', name: 'app_checkout')]
-    public function index(): Response
+    public function index(Request $request, CartRepository $cartRepository): Response
     {
+        $cartId = $request->getSession()->get('cart_id');
+        $cart = $cartRepository->findOneBy([
+            'uuid' => $cartId,
+        ]);
+        $isCartEmpty = null === $cart || null === $cart->getCartItems();
+
         return $this->render('checkout/index.html.twig', [
             'controller_name' => 'CheckoutController',
+            'cart' => $cart,
+            'isCartEmpty' => $isCartEmpty,
         ]);
     }
 }
